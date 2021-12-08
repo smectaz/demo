18ed127d38fb5feb7bd731dccb065fce7f3597fd
smectaz <90684643+smectaz@users.noreply.github.com>
2021-12-08 14:42:45 +0100

    debut de twig

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -5,15 +5,32 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGenerator;
 
 class HelloController extends AbstractController
 {
-    #[Route('/bonjour/{name}', name:'hello')]
-function index(string $name): Response
+    #[Route('/autre/{name}', methods:['GET'], requirements:['name' => '[a-z]+'], name:'hello')]
+    
+public function index(string $name): Response
     {
-    dump($name);
+
+    dump($this->generateUrl(
+        'hello',
+        ['name' => 'random', 'more' => 'kkk'],
+        UrlGenerator::ABSOLUTE_URL
+    ));
+
     return $this->render('hello/index.html.twig', [
-        'controller_name' => 'HelloController',
+        'name' => $name,
+        'key' => 'first',
+        'articles' => [
+            'title' => 'sample',
+        ],
+        'student' => [
+            'firstname' => 'cedric',
+            'lastname' => $name,
+        ],
+        'date' => new \DateTime(),
     ]);
 }
 }
